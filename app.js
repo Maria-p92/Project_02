a7dbec9072be78d829e99fc6e018a3f9bf8acd93
Paul <n.d.mont.p@gmail.com>
2021-02-08 17:07:03 +0100

    all-recipes1

---
+++ app.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 30,
+    dots: true,
+    nav: true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        },
+        2000:{
+            items:3
+        }
+    }
+})
+
